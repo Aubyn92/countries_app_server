68c0cb631e957609210ff314dc5716af359aa8c4
Aubyn92 <livstaubyn92@gmail.com>
2020-06-30 17:59:56 +1000

    Created crud actions and params in countries controller. Set up rendering of json with status codes.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,37 @@
 class CountriesController < ApplicationController
+    before_action :set_country, only: [:show, :update, :destroy]
+
+    def index 
+        @countries = Country.all
+        render json: @countries
+      end 
+    
+    def show 
+       render json: @country
+    end 
+
+    def create 
+        Country.create(country_params)
+        render json: "country created", status: 201 
+    end 
+
+    def update 
+        @country.update(country_params)
+        render json: "country updated", status: 200
+    end 
+
+    def destroy
+        @country.destroy
+        render json: "country deleted", status: 200
+    end 
+
+    private 
+
+    def country_params 
+        params.require(:country).permit(:name, :description, :airline, :year)
+    end 
+
+    def set_country 
+        @country = Country.find(params[:id]) 
+    end
 end
